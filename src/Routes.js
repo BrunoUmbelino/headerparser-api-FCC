115d16c771865252379ad854107c1e41fe23bece
Bruno <bruno.umbelino0@gmail.com>
2021-06-20 12:03:02 -0300

    add functionality that returns customer information collected with the request header

---
+++ src/Routes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const Routes = express.Router();
+
+Routes.get("/api/hello", function (req, res) {
+  res.json({ greetings: "Hello" });
+});
+
+Routes.get("/api/whoami", (req, res) => {
+  const headers = req.headers;
+
+  const host = headers.host;
+  const userAgent = headers["user-agent"];
+  const acceptLanguage = headers["accept-language"];
+
+  res.json({ ipAdress: host, software: userAgent, language: acceptLanguage });
+});
+
+module.exports = Routes;
