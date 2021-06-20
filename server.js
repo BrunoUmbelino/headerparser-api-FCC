b801a96da6a8d7504fc7dfdcd0894c884b50baf2
Bruno <bruno.umbelino0@gmail.com>
2021-06-20 12:08:16 -0300

    removing unused import

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const Routes = require("./src/Routes");
+require("dotenv").config();
 // server.js
 // where your node app starts
 
 // init project
-require("dotenv").config();
+
 var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require("cors");
-const { route } = require("./src/Routes");
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 app.use(Routes);
 
